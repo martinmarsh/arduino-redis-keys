f2b1b4bc897c883c7ab7db5666e69166cb1536fb
mm <martin@marshtrio.co.uk>
2020-03-07 18:33:22 +0000

    refactoring

---
+++ src/redis-keys.cpp
@@ -0,0 +1,162 @@
+
+#ifndef redis_keys
+
+#include "Arduino.h"
+#include "redis-keys.h"
+#endif
+
+void Redis::set_callbacks(func print, func println){
+    call_back.print = print;
+    call_back.println = println;
+    call_back.enable = false;
+}
+
+void Redis::set_callbacks(func print, func println, func1 call_func){
+    call_back.print = print;
+    call_back.println = println;
+    call_back.reply = call_func;
+    call_back.enable = true;
+}
+
+void Redis::disable_reply_callback(){
+    call_back.enable = false;
+}
+
+void Redis::enable_reply_callback(){
+    call_back.enable = true;
+}
+
+byte Redis::data_name(String name){
+    data_defs[index].name = name;
+    return index++;
+}
+
+byte Redis::data(String name, float* var){
+    data_defs[index].type = is_float;
+    data_defs[index].pvar.pfloat = var;
+    return data_name(name); 
+}
+
+byte Redis::data(String name, byte* var){
+    data_defs[index].type = is_byte;
+    data_defs[index].pvar.pbyte = var;
+    return data_name(name); 
+}
+
+byte Redis::data(String name, int* var){
+    data_defs[index].type = is_int;
+    data_defs[index].pvar.pint = var;
+    return data_name(name); ;
+    
+}
+
+byte Redis::data(String name, unsigned int* var){
+    data_defs[index].type = is_uint;
+    data_defs[index].pvar.puint = var;
+    return data_name(name); ;
+    
+}
+
+byte Redis::data(String name, long* var){
+    data_defs[index].type = is_long;
+    data_defs[index].pvar.plong = var;
+    return data_name(name);
+    
+}
+
+byte Redis::data(String name, unsigned long* var){
+    data_defs[index].type = is_ulong;
+    data_defs[index].pvar.pulong = var;
+    return data_name(name);
+    
+}
+
+byte Redis::data(String name, double* var){
+    data_defs[index].type = is_double;
+    data_defs[index].pvar.pdouble = var;
+    return data_name(name);
+    
+}
+
+byte Redis::data(String name, char* var){
+    data_defs[index].type = is_char;
+    data_defs[index].pvar.pchar = var;
+    return data_name(name);
+    
+}
+
+
+void Redis::convert(byte i, char* pReply){
+  switch(data_defs[i].type){
+    case is_byte:
+      *data_defs[i].pvar.pbyte = atoi(pReply);
+      break;
+    case is_int:
+      *data_defs[i].pvar.pint = atoi(pReply);
+      break;
+    case is_uint:
+      *data_defs[i].pvar.puint = atoi(pReply);
+      break;
+    case is_float:                           
+      *data_defs[i].pvar.pfloat = atof(pReply);
+      break;
+    case is_double:
+      *data_defs[i].pvar.pdouble = atof(pReply);
+      break;
+    case is_long:
+      *data_defs[i].pvar.plong = atol(pReply);
+      break;
+    case is_ulong:
+      *data_defs[i].pvar.pulong = atol(pReply);
+      break;
+    case is_char:
+      *data_defs[i].pvar.pulong = *pReply;
+  };              
+  
+}
+
+void Redis::sync(char c){
+  byte i;
+
+  if (c == 0x0D){
+      rec_time = micros();
+      if ((reply[0] == '$' || reply[0] == '+') && reply[1] == '@' ){
+        var_follows = reply[2];
+        convert(i, pReply+3); 
+        pReply = reply; 
+      }else{
+        *pReply = '\0'; 
+          pReply = reply; 
+        if(var_follows >= 0){
+          convert(var_follows, pReply);
+        }
+        if(call_back.enable == true){
+            call_back.reply(pReply);
+        }      
+        var_follows = -1;
+      }
+    
+    }else if(c != 0x0A){
+    *pReply++ = c;
+  }   
+
+}
+
+
+void Redis::set(byte ref){        
+    call_back.print("set");
+    call_back.print(" ");
+    call_back.print(data_defs[ref].name);
+    call_back.print(" ");
+    //call_back.println(*data_defs[ref].pvar.pfloat);
+}
+
+void Redis::get(byte ref){ 
+    call_back.print("Echo @");
+    //call_back.println((char)ref);       
+    call_back.print("get");
+    call_back.print(" ");
+    call_back.print(data_defs[ref].name);
+}
+
+
---
+++ src/redis-keys.h
@@ -0,0 +1,84 @@
+/*
+  redis_shared_keys.h - Minimal Non blocking Library for
+  sharing keys with a redis server.
+  Created by Martin D Marsh, Marsh 07, 2020.
+  Released into the public domain.
+*/
+#ifndef redis_keys
+
+#include "Arduino.h"
+
+union pval {
+          byte* pbyte;
+          int* pint;
+          unsigned int* puint;
+          float* pfloat;
+          double* pdouble;
+          long* plong;
+          unsigned long* pulong;
+          char* pchar;  
+      } pvals;
+
+enum val_type { is_byte=0, is_int, is_uint, is_float, is_double, is_long, is_ulong, is_char } types;
+
+struct shared_data
+    { 
+      String name;
+      val_type type;
+      pval  pvar;
+    };
+
+
+ shared_data data_defs[15];
+
+class Redis{
+  private:
+    char *pReply = reply;
+       
+  public:
+    char reply[40];
+    byte has_connected;
+    int reply_count;
+    int reply_status;
+    unsigned long rec_time = 0;
+    void redis_callback(char* reply);
+    byte index = 0;
+    int var_follows = -1;
+
+    typedef void func(String print);
+    typedef void func1(char* reply);
+    
+    struct redis_callbacks
+    { 
+      void (*print)(String print);
+      void (*println)(String print);
+      bool enable; 
+      void (*reply)(char* reply);
+    };
+
+    redis_callbacks call_back;
+
+    void enable_reply_callback();
+    void disable_reply_callback();
+
+    void set_callbacks(func print_func, func println_func);
+    void set_callbacks(func print_func, func println_func, func1 call_func);
+
+    byte data_name(String name);
+    byte data(String name, float* var);
+    byte data(String name, byte* var);
+    byte data(String name, int* var);
+    byte data(String name, unsigned int* var);
+    byte data(String name, long* var);
+    byte data(String name, unsigned long* var);
+    byte data(String name, double* var);
+    byte data(String name, char* var);
+
+    void convert(byte i, char* pReply);
+    void sync(char c);
+    void set(byte ref);
+    void get(byte ref);
+};
+      
+
+#endif
